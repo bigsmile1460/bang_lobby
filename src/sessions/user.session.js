72232cacaee72230d4d7a759f52ce39f35199ba4
bigsmile1460 <wns4711@naver.com>
2024-12-11 21:47:53 +0900

    fix: 불필요한 주석 제거
    Co-authored-by: sdh02142 <sdh02142@users.noreply.github.com>
    Co-authored-by: LEE, DONGEUN <armsterd@users.noreply.github.com>
    Co-authored-by: Freak-J <Freak-J@users.noreply.github.com>
    Co-authored-by: doughnutdough5 <doughnutdough5@users.noreply.github.com>

--- src/sessions/user.session.js
+++ src/sessions/user.session.js
@@ -14,28 +14,5 @@ export const findUserById = async (key) => {
 };
 
 export const getUser = async (key) => {
-  return await redis.getHash('user',key);
+  return await redis.getHash('user', key);
 };
-/////////////////// redis 변경 전
-// export const addUser = (user) => {
-//   userSession.push(user);
-// };
-
-// export const removeUser = (socket) => {
-//   const index = userSession.findIndex((user) => user.socket === socket);
-//   // 못 찾은 경우
-//   if (index === -1) {
-//     console.error('유저를 찾지 못했습니다.');
-//     return null;
-//   }
-
-//   return userSession.splice(index, 1)[0];
-// };
-
-// export const findUserById = (id) => {
-//   return userSession.find((user) => user.id === id);
-// };
-
-// export const getUserBySocket = (socket) => {
-//   return userSession.find((user) => user.socket === socket);
-// };
