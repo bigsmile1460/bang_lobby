969537c245d2a41f68f05bee9c64d92f4c5b7540
bigsmile1460 <wns4711@naver.com>
2024-12-11 14:46:23 +0900

    fix: 사용함수 변경
    Co-authored-by: sdh02142 <sdh02142@users.noreply.github.com>
    Co-authored-by: Freak-J <Freak-J@users.noreply.github.com>
    Co-authored-by: LEE, DONGEUN <armsterd@users.noreply.github.com>

--- src/sessions/user.session.js
+++ src/sessions/user.session.js
@@ -1,22 +1,21 @@
 import RedisManager from '../classes/manager/redis.manager.js';
 
 const redis = RedisManager.getInstance();
-export const addUser = async (key,user) => {
-  await redis.setUser(key, user);
-}; 
+export const addUser = async (key, user) => {
+  await redis.setHash('user', key, JSON.stringify(user));
+};
 
 export const removeUser = async (key) => {
-  await redis.delUser(key);
+  await redis.delHash('user',key);
 };
 
 export const findUserById = async (key) => {
-  return await redis.getUser(key)
+  return await redis.getHash('user',key);
 };
 
 export const getUser = async (key) => {
-  return await redis.getUser(key)
+  return await redis.getHash('user',key);
 };
-
 /////////////////// redis 변경 전
 // export const addUser = (user) => {
 //   userSession.push(user);
