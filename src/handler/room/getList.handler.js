8043fbc611de347f2eb95265d40ee65586cabeda
bigsmile1460 <wns4711@naver.com>
2024-12-11 21:39:54 +0900

    fix: JSON데이터 파싱 방법 변경
    Co-authored-by: sdh02142 <sdh02142@users.noreply.github.com>
    Co-authored-by: LEE, DONGEUN <armsterd@users.noreply.github.com>
    Co-authored-by: Freak-J <Freak-J@users.noreply.github.com>
    Co-authored-by: doughnutdough5 <doughnutdough5@users.noreply.github.com>

--- src/handler/room/getList.handler.js
+++ src/handler/room/getList.handler.js
@@ -1,15 +1,14 @@
 import RedisManager from '../../classes/manager/redis.manager.js';
 import { PACKET_TYPE } from '../../constants/header.js';
-import { getAllGameSessions } from '../../sessions/game.session.js';
 import { createResponse } from '../../utils/response/createResponse.js';
 
 const getRoomListHandler = async (socket, payload) => {
   const redis = RedisManager.getInstance();
-  const rooms = JSON.parse(await redis.getHvals('room'));
-
+  const roomData = await redis.getHvals('room');
+  const rooms = roomData.map((data) => JSON.parse(data));
   const responsePayload = {
     getRoomListResponse: {
-      rooms: [rooms],
+      rooms: rooms,
     },
   };
 
