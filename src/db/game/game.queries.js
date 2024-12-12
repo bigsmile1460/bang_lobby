75cd83fcd1624e276fa152956199ee344b39cd03
bigsmile1460 <wns4711@naver.com>
2024-12-12 13:24:48 +0900

    fix:  필드명 수정
    Co-authored-by: sdh02142 <sdh02142@users.noreply.github.com>
    Co-authored-by: LEE, DONGEUN <armsterd@users.noreply.github.com>
    Co-authored-by: Freak-J <Freak-J@users.noreply.github.com>

--- src/db/game/game.queries.js
+++ src/db/game/game.queries.js
@@ -1,6 +1,6 @@
 export const SQL_QUERIES = {
   LOAD_LOCATIONS: 'SELECT * FROM SpawnLocation',
-  LOAD_CARDS: 'SELECT card_type, count FROM card_deck ORDER BY card_type;',
+  LOAD_CARDS: 'SELECT card_type_id, count FROM card_deck ORDER BY card_type_id;',
   LOAD_ROLES: 'SELECT rd.user_count, r.id AS role_id, rd.role_count FROM role_distribution rd JOIN roles r ON rd.role_id = r.id ORDER BY rd.user_count, r.id;',
-  LOAD_CHARACTERS: 'SELECT id, type FROM characters ORDER BY id;',
+  LOAD_CHARACTERS: 'SELECT id FROM characters ORDER BY id;',
 };
