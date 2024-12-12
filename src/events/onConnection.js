239cf87ff9019c738004fc476df22ecfd8e3bcf1
bigsmile1460 <wns4711@naver.com>
2024-12-12 13:25:36 +0900

    fix: 로비서버에서 불필요한 부분 주석 처리
    Co-authored-by: sdh02142 <sdh02142@users.noreply.github.com>
    Co-authored-by: LEE, DONGEUN <armsterd@users.noreply.github.com>
    Co-authored-by: Freak-J <Freak-J@users.noreply.github.com>

--- src/events/onConnection.js
+++ src/events/onConnection.js
@@ -1,4 +1,3 @@
-import RedisManager from '../classes/manager/redis.manager.js';
 import { onData } from './onData.js';
 import { onEnd } from './onEnd.js';
 import { onError } from './onError.js';
@@ -10,16 +9,15 @@ export const onConnection = (socket) => {
 
   
   // 클라이언트 연결 시 소켓 정보 추출
-  const clientInfo = {
-    address: socket.remoteAddress,
-    port: socket.remotePort,
-    jwt: socket.jwt, // 고유 식별자
-  };
-  const redis = RedisManager.getInstance();
+  // const clientInfo = {
+  //   address: socket.remoteAddress,
+  //   port: socket.remotePort,
+  //   jwt: socket.jwt, // 고유 식별자
+  // };
+  
   // Redis에 소켓 정보 저장
   // TODO: 확인로직이 필요함
-  redis.set(clientInfo.jwt, JSON.stringify(clientInfo));
-
+  // redis.set(clnfo.jwt, JSON.stringify(clientInfo));
 
   socket.on('data', onData(socket));
   socket.on('end', onEnd(socket));
