5c4771b9fbf445d9bd7b257fcf9985d1db979b0d
bigsmile1460 <wns4711@naver.com>
2024-12-16 21:20:59 +0900

    fix: 동기 코드로 변환
    Co-authored-by: sdh02142 <sdh02142@users.noreply.github.com>
    Co-authored-by: Freak-J <Freak-J@users.noreply.github.com>
    Co-authored-by: LEE, DONGEUN <armsterd@users.noreply.github.com>
    Co-authored-by: doughnutdough5 <doughnutdough5@users.noreply.github.com>

--- src/utils/shuffle.js
+++ src/utils/shuffle.js
@@ -12,33 +12,41 @@ const shuffle = (array) => {
 export const setUpGame = async (room) => {
   const redis = RedisManager.getInstance();
 
+  // roleTypes, cardDeck, characterList를 동기적으로 가져오기
   const roleTypes = await redis.get('roleTypes');
   const cardDeck = await redis.get('cardDeck');
   const characterList = await redis.get('characterList');
 
-  
   // roleType 배분
   const roleTypeClone = roleTypes[room.users.length];
-  const shuffledRoleType = shuffle(roleTypeClone);
-  room.users.forEach((user, i) => {
+  const shuffledRoleType = shuffle(roleTypeClone); // shuffle이 비동기 함수가 아니므로 await 제거
+  
+  for (let i = 0; i < room.users.length; i++) {
+    const user = room.users[i];
     user.setCharacterRoleType(shuffledRoleType[i]);
     if (user.characterData.roleType === Packets.RoleType.TARGET) {
       user.increaseHp();
     }
-  });
+  }
 
   // 캐릭터 배분
   const shuffledCharacter = shuffle(characterList).splice(0, room.users.length);
-  room.users.forEach((user, i) => {
+  for (let i = 0; i < room.users.length; i++) {
+    const user = room.users[i];
     user.setCharacter(shuffledCharacter[i].type);
-  });
+  }
 
+  // 카드 덱 셔플
   const deck = shuffle(cardDeck);
 
   // 카드 배분
-  room.users.forEach((user) => {
+  for (const user of room.users) {
     const gainCards = deck.splice(0, user.characterData.hp);
-    gainCards.forEach((card) => user.addHandCard(card));
-  });
+    for (const card of gainCards) {
+      user.addHandCard(card);
+    }
+  }
+
+  // 덱 업데이트
   room.deck = deck;
 };
